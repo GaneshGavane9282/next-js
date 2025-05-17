0461d231d97da4ce45c0965ed17b7f988622b12e
@ganesh.cg <you@example.com>
2025-05-17 22:15:19 +0530

    Add Custom not found page and Add custom not found page on the basis of content

---
+++ src/app/not-found.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <>
+      <h1>404 Not Found</h1>
+      <p>Requested Resource is not available</p>
+    </>
+  );
+};
+
+export default NotFound;
