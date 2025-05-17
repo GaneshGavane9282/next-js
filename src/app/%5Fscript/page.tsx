b326b7a0daaba83faa9fd9d1c10b6771ce05a9c7
@ganesh.cg <you@example.com>
2025-05-17 22:16:24 +0530

    Add Private Folders and Add underscore at URL by using %5F

---
+++ src/app/%5Fscript/page.tsx
@@ -0,0 +1,6 @@
+const Script = () => {
+  return <h1>Welcome to Script Landing Page</h1>;
+};
+
+export default Script;
+  
---
+++ src/app/_lib/page.tsx
@@ -0,0 +1,5 @@
+const Lib = () => {
+  return <div>Welcome to Lib</div>;
+};
+
+export default Lib;
